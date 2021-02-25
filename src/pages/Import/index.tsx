b60b0cec3822b29271cddaa797e41ad47c050432
Guilherme Carmona <guilhermecarmona@gmail.com>
2021-02-25 01:12:05 -0300

    initial commit

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,28 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
+    for (const uploadedFile of uploadedFiles) {
+      data.append('files', uploadedFile.file);
+    }
 
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const filesWithProps: FileProps[] = files.map(file => {
+      return {
+        file,
+        name: file.name,
+        readableSize: filesize(file.size),
+      };
+    });
+    setUploadedFiles([...uploadedFiles, ...filesWithProps]);
   }
 
   return (
